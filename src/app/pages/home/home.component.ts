deac3c033a6038492a63a542ce506c476d91a3f2
epo <epolixa@gmail.com>
2020-01-13 20:08:58 -0500

    add storage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'bityard-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
--- src/app/pieces/header/header.component.ts
+++ src/app/pieces/header/header.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
+  selector: 'bityard-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
 
+	private titleText: string; 
+	private bannerSource: string;
+	private taglineText: string;
+
   constructor() { }
 
   ngOnInit() {
+  	this.titleText = "Bityard";
+  	this.bannerSource = "assets/images/header.jpg";
+  	this.taglineText= "This little light of mine";
   }
 
 }
