5b4bfa420426888b4049ac7f250b1b4774389d0e
epo <epolixa@gmail.com>
2020-01-13 19:16:08 -0500

    base commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './pieces/header/header.component';
+import { FooterComponent } from './pieces/footer/footer.component';
+import { NavigationComponent } from './pieces/navigation/navigation.component';
+
+const appRoutes: Routes = [
+	{path: '', redirectTo: '/home', pathMatch: 'full'},
+	{path: 'home', component: HomeComponent}
+];
+
+@NgModule({
+	declarations: [
+		AppComponent,
+		HomeComponent,
+		HeaderComponent,
+		FooterComponent,
+		NavigationComponent,
+	],
+	imports: [
+		RouterModule.forRoot(appRoutes, {enableTracing: true}),
+		BrowserModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
+})
+export class AppModule { }
