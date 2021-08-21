56a1c82800d7cebb8ead0a417ec1aa1a0c62967a
epo <epolixa@gmail.com>
2021-08-20 20:45:38 -0400

    add crafatar endpoint to minecraft service

--- src/app/minecraft.service.ts
+++ src/app/minecraft.service.ts
@@ -8,13 +8,17 @@ import frens from '../assets/data/frens.json';
 })
 export class MinecraftService {
 
-  // https://api.mcsrvstat.us/
-  private serverStatusUrl:string = "https://api.mcsrvstat.us/2/";
+  private serverStatusUrl:string = "https://api.mcsrvstat.us/2/"; // https://api.mcsrvstat.us/
+  private avatarUrl:string = "https://crafatar.com/avatars/"; // https://crafatar.com/
 
   constructor(private http:HttpClient) {}
 
-  getServerStatus(address:any): Observable<any> {
+  getServerStatus(address:string): Observable<any> {
     return this.http.get(this.serverStatusUrl + address);
+  }
+
+  getAvatar(uuid:string): Observable<any> {
+    return this.http.get(this.avatarUrl + uuid);
   }
 
 }
--- src/app/pages/minecraft/minecraft.component.ts
+++ src/app/pages/minecraft/minecraft.component.ts
@@ -12,11 +12,20 @@ export class MinecraftComponent implements OnInit {
   private showBluemap: boolean = false;
 
   private minecraftServerStatusSub:Subscription;
+  private minecraftAvatarSub:Subscription;
 
   constructor(private minecraftService:MinecraftService) { }
 
   ngOnInit() {
-    this.minecraftServerStatusSub = this.minecraftService.getServerStatus("104.243.41.80").subscribe();
+    this.minecraftServerStatusSub = this.minecraftService.getServerStatus("104.243.41.80").subscribe(response => {
+      let uuid = response.players.uuid;
+      if (uuid) {
+        let uuids = Object.values(uuid);
+        for (let u of uuids) {
+          this.minecraftAvatarSub = this.minecraftService.getAvatar(u).subscribe();
+        }
+      }
+    });
   }
 
 }
