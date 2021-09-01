d8681a92e2b3f0ba4c233aee770674213f18b2fc
epo <epolixa@gmail.com>
2021-08-31 21:21:56 -0400

    rough navigation styles

--- src/app/pieces/navigation/navigation.component.ts
+++ src/app/pieces/navigation/navigation.component.ts
@@ -1,24 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'bityard-navigation',
-  templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+	selector: 'bityard-navigation',
+	templateUrl: './navigation.component.html',
+	styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
 
-	private links:{label:string,route:string}[];
+	private links: { label: string, route: string }[];
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  	this.links = [
-  		{label:"Home",route:""},
-  		{label:"About",route:"about"},
-  		{label:"Minecraft",route:"minecraft"},
-  		{label:"Live",route:"live"},
-  		{label:"Downloads",route:"downloads"}
-  	];
-  }
+	ngOnInit() {
+		this.links = [
+			{ label: "Home", route: "" },
+			{ label: "About", route: "about" },
+			{ label: "Minecraft", route: "minecraft" },
+			{ label: "Live", route: "live" },
+			{ label: "Downloads", route: "downloads" }
+		];
+	}
 
 }
