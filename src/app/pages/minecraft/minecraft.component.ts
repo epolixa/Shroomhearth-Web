53d75d89998e945d7c17806774b13f7e424b38d4
epolixa <epolixa@gmail.com>
2022-03-31 11:46:06 -0400

    minecraft page updates

--- src/app/pages/minecraft/minecraft.component.ts
+++ src/app/pages/minecraft/minecraft.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { MinecraftService } from '../../minecraft.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser'; 
+import { ScriptService } from 'src/app/script.service';
 
 @Component({
   selector: 'app-minecraft',
@@ -12,18 +14,31 @@ export class MinecraftComponent implements OnInit {
   public pageTitle:string = "Minecraft";
 
   private showBluemap: boolean = false;
+  private ip: string;
   private version: string;
+  private online: boolean;
   private avatars: any[];
+  private bluemapUrl: string;
+  private safeBluemapUrl: SafeResourceUrl;
 
   private minecraftServerStatusSub:Subscription;
   private minecraftAvatarSub:Subscription;
 
-  constructor(private minecraftService:MinecraftService) { }
+  constructor(
+    private minecraftService:MinecraftService,
+    private script:ScriptService,
+    private sanitizer:DomSanitizer
+    ) {
+    this.ip = this.minecraftService.getAddress();
+    this.bluemapUrl = "http://" + this.ip + ":25570/";
+    this.safeBluemapUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.bluemapUrl);
+  }
 
   ngOnInit() {
     this.avatars = [];
     this.minecraftServerStatusSub = this.minecraftService.getServerStatus().subscribe(response => {
       this.version = response.version;
+      this.online = response.online;
       let uuid = response.players.uuid;
       if (uuid) {
         for (let u in uuid) {
@@ -42,6 +57,12 @@ export class MinecraftComponent implements OnInit {
         }
       }
     });
+
+    this.script.load('trello').then(data => {console.log("trello script loaded", data)}).catch(error => console.log(error));
+  }
+
+  public toggleBluemap() {
+    this.showBluemap = !this.showBluemap;
   }
 
 }
