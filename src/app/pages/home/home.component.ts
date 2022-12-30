0ab7e702258ae500f350b66bcd42ee08c4389803
epolixa <epolixa@gmail.com>
2022-12-30 16:06:50 -0500

    add captions to homepage slideshow

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ShroomhearthService } from 'src/app/shroomhearth.service';
 
+import home_slideshow_captions from 'src/assets/data/home_slideshow_captions.json';
+
 @Component({
   selector: 'shroomhearth-home',
   templateUrl: './home.component.html',
@@ -27,7 +29,10 @@ export class HomeComponent implements OnInit {
 
     // init slides
     for (let i = 1; i <= 30; i++) {
-      this.slides.push({img: "assets/images/home_slideshow/slide_" + i + ".png"});
+      this.slides.push({
+        img: "assets/images/home_slideshow/slide_" + i + ".png",
+        caption: home_slideshow_captions["slide_" + i]
+      });
     }
     this.slides = this.shroomhearth.shuffleArray(this.slides);
   }
